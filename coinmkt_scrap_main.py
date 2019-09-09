56705a12021ba47e2372e80db435b66ae5d73567
ashishde1991 <40660263+ashishde1991@users.noreply.github.com>
2019-09-09 13:21:52 +0530

    Add files via upload

---
+++ coinmkt_scrap_main.py
@@ -0,0 +1,117 @@
+import requests
+import lxml.html as lh
+import pandas as pd
+import urllib.request
+from bs4 import BeautifulSoup
+import datetime
+from datetime import date
+import pandas as pd
+import re
+import threading
+from time import sleep , time
+import logging 
+
+logging.basicConfig(level=logging.DEBUG)
+
+r = requests.get('https://coinmarketcap.com/all/views/all/')
+soup = BeautifulSoup(r.text, 'html.parser')
+
+
+
+data = []
+table = soup.find('table', id='currencies-all')
+for row in table.find_all('tr'):
+    try:
+        Name = row.find('a',class_='currency-name-container link-secondary').text
+        symbol = row.find('td', class_='text-left col-symbol').text
+        price = row.find('a', class_='price').text
+        MarketCap = row.find('td',class_ ='market-cap').text
+        Circulating_Supply = row.find('td',class_ = 'circulating-supply').text
+        Volume = row.find('a',class_='volume').text
+    except AttributeError:
+        continue
+    data.append(( Name,symbol, price,MarketCap,Circulating_Supply,Volume))
+    
+
+df = pd.DataFrame(data)
+df.columns = [ 'Name','Symbol','Price','MarketCap','Circulating_Supply','Volume']
+
+df['Name'] = df['Name'].str.replace(" ","-")
+#df['Name'] = df['Name'].str.replace("..","")
+
+#df1['State'] = df1['State'].str.replace(" ","")
+
+#export_excel = df.to_excel(r"C:\Users\admin\Desktop\python scrapping\Coinmarket_scrapp\CoinMarket.xlsx", header=True) 
+import traceback
+
+df.Name[3]
+x = []
+def coin(Name,df):
+#    q = ''
+    try:
+        q = requests.get(f'https://coinmarketcap.com/currencies/{Name}')
+        soup2 = BeautifulSoup(q.text)
+   #     print(q.text)
+        l = soup2.find_all('span', {"title":"Website"})[0].parent.findChildren()[1]["href"]
+        x.append(l)
+        logging.debug(f"got mail of {Name} ")
+        sleep(10)
+    except:
+        traceback.print_exc()
+        logging.debug(f"None for {Name} ")
+        l = "None"
+        
+        x.append(l)
+        
+        
+
+
+df1 = {}
+#cnt =0
+for i in df.Name:
+    coin(i,df1)
+#    cnt+=1
+#    if cnt == 30 : break
+#   
+print (x)
+df1 = pd.DataFrame(x)
+df1.columns = ['website']
+
+
+df_final = pd.concat([df,df1],axis=1)
+
+export_excel = df_final.to_excel(r"C:\Users\admin\Desktop\python scrapping\Coinmarket_scrapp\coimktall_scrapp.xlsx", header=True) 
+
+
+'''
+
+#
+#thread1 = threading.Thread(target = coin, args = (i,df1))
+#thread1.start()
+
+exit()
+#
+#if __name__ == "__main__":
+#    coin(Name,df1)
+
+
+
+
+for i in df.Currency_Name['i']:
+    
+    q = requests.get(f'https://coinmarketcap.com/currencies/{df.Currency_Name["i"]}')
+    soup2 = BeautifulSoup(q.text, 'html.parser')
+    #link = soup.find_all('a')
+    l = soup2.find_all('span', {"title":"Website"})[0].parent.findChildren()[1]["href"]
+    i=i+1
+    x.append(l)
+df1 = pd.Dataframe(x)
+df1.columns = ['email_id']
+'''
+    
+
+    
+
+        
+        
+        
